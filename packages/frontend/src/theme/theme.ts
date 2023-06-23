f980836a05e66f0682a1a8d26161f10536e37079
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-23 09:56:50 +0100

    feat: system color scheme

--- packages/frontend/src/theme/theme.ts
+++ packages/frontend/src/theme/theme.ts
@@ -1,8 +1,13 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 import colors from './colors'
+
+const config: ThemeConfig = {
+  useSystemColorMode: true
+}
 
 const theme = extendTheme({
   colors,
+  config,
   fonts: {
     heading: `'Inter', sans-serif`,
     body: `'Inter', sans-serif`
