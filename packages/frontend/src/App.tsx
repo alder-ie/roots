a7b0108aa8b9f0edb15a4fa412f91857ac49c558
Jordan Harrison <76656637+jouwdan@users.noreply.github.com>
2023-06-23 10:05:49 +0100

    feat: depracate protectedroute in favor of isauthenticated in routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Navigation } from './components/Navigation'
 
-import { ProtectedRoute } from './components/ProtectedRoute'
-
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import PageNotFound from './pages/PageNotFound'
@@ -11,7 +9,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Box, Progress } from '@chakra-ui/react'
 
 const App = () => {
-  const { isLoading } = useAuth0()
+  const { isLoading, isAuthenticated } = useAuth0()
 
   return (
     <>
@@ -23,11 +21,17 @@ const App = () => {
           {!isLoading && (
             <Box px={4} py={2}>
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/profile"
-                  element={<ProtectedRoute page={Profile} />}
-                />
+                {!isAuthenticated && (
+                  <>
+                    <Route path="/" element={<Home />} />
+                  </>
+                )}
+                {isAuthenticated && (
+                  <>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                  </>
+                )}
                 <Route path="*" element={<PageNotFound />} />
               </Routes>
             </Box>
